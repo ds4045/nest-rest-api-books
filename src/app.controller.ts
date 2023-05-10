577ab10c51b149823c3247f2b9bebdb399ee0515
ds4045 <ds4045@mail.ru>
2023-05-10 13:24:04 +0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,9 @@
-import { Controller, Get, UseFilters } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
-import { BadRequestFilter } from './common/request.filter';
 
 @Controller()
 @ApiTags()
-@UseFilters(BadRequestFilter)
 export class AppController {
   constructor(private readonly appService: AppService) {}
   @Get()
